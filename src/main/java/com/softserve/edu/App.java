eb6e1bf951af5c9506089f5a2159938c024481dc
Oleh Kozyr <oleg.kozyr@aer.kpi.ua>
2023-03-26 14:54:57 +0300

    cope files from softserve

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -1,13 +1,31 @@
 package com.softserve.edu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App { // extends Object
+    private int i = 123;
+
+    public void print() {
+        System.out.println("i = " + i);
+    }
+
+    @Override
+    public String toString() {
+        return "App{" +
+                "i=" + i +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println("main i = " + app.i);
+        app.print();
+        //
+        App app2 = new App();
+        app2.i = 456;
+        System.out.println("main i2 = " + app2.i);
+        app2.print();
+        //
+        System.out.println("main i = " + app.i);
+        //
+        System.out.println("app = " + app);
     }
 }
