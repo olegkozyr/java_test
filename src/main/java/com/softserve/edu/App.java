13606fe1a106efc26ba427fab49cd81b87338846
Oleh Kozyr <oleg.kozyr@aer.kpi.ua>
2023-04-01 16:27:21 +0300

    Test email correctnes

--- src/main/java/com/softserve/edu/App.java
+++ src/main/java/com/softserve/edu/App.java
@@ -1,31 +1,58 @@
 package com.softserve.edu;
 
-public class App { // extends Object
-    private int i = 123;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public void print() {
-        System.out.println("i = " + i);
+public class App {
+    private Pattern Regex;
+    public Pattern getPattern() {
+        return Regex;
     }
 
-    @Override
-    public String toString() {
-        return "App{" +
-                "i=" + i +
-                '}';
+    public App() {
+        this.Regex = Pattern.compile("^([a-zA-Z])+\\w*(\\.\\w+)*@(\\w+\\.)+\\w{2,}");
+    }
+    public boolean checkEmail(String text) {
+        if (text == null) { return false; }
+
+        Matcher m = Regex.matcher(text);
+        return  m.matches();
+    }
+
+    public void printMatches(String text) {
+        if (checkEmail(text)) {
+            System.out.println("Matches: TRUE");
+        } else {
+            System.out.println("Matches: FALSE");
+        }
     }
 
     public static void main(String[] args) {
         App app = new App();
-        System.out.println("main i = " + app.i);
-        app.print();
-        //
-        App app2 = new App();
-        app2.i = 456;
-        System.out.println("main i2 = " + app2.i);
-        app2.print();
-        //
-        System.out.println("main i = " + app.i);
-        //
-        System.out.println("app = " + app);
+        System.out.println("Pattern = " + app.getPattern());
+        String text = "a.bd.c@gmail.com";
+        System.out.print(text + " - ");
+        app.printMatches(text);
+        text = "first.second.third@mail.com.ua";
+        System.out.print(text + " - ");
+        app.printMatches(text);
+        text = "a.b.c@gmail.com";
+        System.out.print(text + " - ");
+        app.printMatches(text);
+        text = "a@i.ua";
+        System.out.print(text + " - ");
+        app.printMatches(text);
+        text = "a.@gmail.com";
+        System.out.print(text + " - ");
+        app.printMatches(text);
+        text = "a@gmail.a";
+        System.out.print(text + " - ");
+        app.printMatches(text);
+        text = "a@gmail";
+        System.out.print(text + " - ");
+        app.printMatches(text);
+        text = "@gmail.ua";
+        System.out.print(text + " - ");
+        app.printMatches(text);
     }
 }
--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -1,22 +1,111 @@
 package com.softserve.edu;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import javafx.scene.layout.Priority;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
+    private static App app;
 
-    @Test
-    public void testApp1() {
-        System.out.println("class AppTest: testApp1()");
-        Assert.assertTrue(true);
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        System.out.println("@BeforeClass setUpBeforeClass()");
+        app = new App();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        System.out.println("@AfterClass tearDownAfterClass()");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("\t@Before setUp()");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("\t@After tearDown()");
     }
 
     @Test
-    public void testApp2() {
-        System.out.println("class AppTest: testApp2()");
-        Assert.assertTrue(true);
+    public void testCorrectEmail() {
+        System.out.println("\t\t@Test testCorrectEmail");
+        boolean actual;
+        boolean expected;
+
+        actual = app.checkEmail("a1.bd.c1@gmail.com");
+        expected = true;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIncorrectEmail() {
+        System.out.println("\t\t@Test testIncorrectEmail()");
+        boolean actual;
+        boolean expected;
+        //
+        actual = app.checkEmail("@gmail");
+        expected = false;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEmailStartWithLetter() {
+        System.out.println("\t\t@Test testEmailStartWithLetter()");
+        boolean actual;
+        boolean expected;
+        //
+        actual = app.checkEmail("1user.name@gmail.com");
+        expected = false;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEmailAtLeastTwoDomainLevels() {
+        System.out.println("\t\t@Test testEmailAtLeastTwoDomainLevels()");
+        boolean actual;
+        boolean expected;
+        //
+        actual = app.checkEmail("user.name@gmail");
+        expected = false;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEmailTopLevelSomainAtLeastTwoLettersLong() {
+        System.out.println("\t\t@Test testEmailTopLevelSomainAtLeastTwoLettersLong()");
+        boolean actual;
+        boolean expected;
+        //
+        actual = app.checkEmail("user.name@gmail.a");
+        expected = false;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEmptyString() {
+        System.out.println("\t\t@Test testEmptyString()");
+        boolean actual;
+        boolean expected;
+        //
+        actual = app.checkEmail("");
+        expected = false;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNull() {
+        System.out.println("\t\t@Test testNull()");
+        boolean actual;
+        boolean expected;
+        //
+        actual = app.checkEmail(null);
+        expected = false;
+        Assert.assertEquals(expected, actual);
     }
 }
